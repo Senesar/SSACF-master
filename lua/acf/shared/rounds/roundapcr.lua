9be9b51ecdd63c7be2e99d45a4db281ca0f849fc
jawn <nukey111@gmail.com>
2018-12-03 22:27:46 -0500

    cxf
    sdf

--- lua/acf/shared/rounds/roundapcr.lua
+++ lua/acf/shared/rounds/roundapcr.lua
@@ -6,10 +6,10 @@ ACF.AmmoBlacklist.AP = { "MO", "SL" }
 local Round = {}
 
 Round.type = "Ammo" --Tells the spawn menu what entity to spawn
-Round.name = "Armour Piercing Composite Rigid (APCR) --Human readable name
+Round.name = "Armour Piercing Composite Rigid (APCR)" --Human readable name
 Round.model = "models/munitions/round_100mm_shot.mdl" --Shell flight model
 Round.desc = "A high-velocity ammo meant to penetrate very thick, flat armor"
-Round.netid = 11 --Unique ammotype ID for network transmission
+Round.netid = 7 --Unique ammotype ID for network transmission
 
 function Round.create( Gun, BulletData )
 	
